a6a60d99d79d8765ad3a139b46a0ca94a583b11f
asvitaliii <a.s.vitaliii@gmail.com>
2020-10-11 21:39:17 +0300

    ver. 2.0

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QMessageBox
 
-from new_fraction import *
+from new_fraction import NewFraction
 
 
 class Window(QtWidgets.QMainWindow):
@@ -9,32 +9,32 @@ class Window(QtWidgets.QMainWindow):
         super().__init__()
         self.__win = uic.loadUi('gui.ui')
 
-    def set_slots(self):
-        self.__win.ButtonSum.clicked.connect(self.sum)
-        self.__win.ButtonSub.clicked.connect(self.sub)
-        self.__win.ButtonMul.clicked.connect(self.mul)
-        self.__win.ButtonDiv.clicked.connect(self.div)
+    def __set_slots(self):
+        self.__win.ButtonSum.clicked.connect(self.__sum)
+        self.__win.ButtonSub.clicked.connect(self.__sub)
+        self.__win.ButtonMul.clicked.connect(self.__mul)
+        self.__win.ButtonDiv.clicked.connect(self.__div)
 
     def show(self):
-        self.set_slots()
+        self.__set_slots()
         self.__win.show()
 
-    def sum(self):
+    def __sum(self):
         a = NewFraction(self.__win.lineEdit.text())
         b = NewFraction(self.__win.lineEdit_2.text())
         QMessageBox.information(self, 'Результат:', f'{a} + {b} = {a + b}')
 
-    def sub(self):
+    def __sub(self):
         a = NewFraction(self.__win.lineEdit.text())
         b = NewFraction(self.__win.lineEdit_2.text())
         QMessageBox.information(self, 'Результат:', f'{a} - {b} = {a - b}')
 
-    def mul(self):
+    def __mul(self):
         a = NewFraction(self.__win.lineEdit.text())
         b = NewFraction(self.__win.lineEdit_2.text())
         QMessageBox.information(self, 'Результат:', f'{a} * {b} = {a * b}')
 
-    def div(self):
+    def __div(self):
         a = NewFraction(self.__win.lineEdit.text())
         b = NewFraction(self.__win.lineEdit_2.text())
         QMessageBox.information(self, 'Результат:', f'{a} / {b} = {a / b}')
--- new_fraction.py
+++ new_fraction.py
@@ -1,9 +1,26 @@
-from fractions import Fraction
+class NewFraction(object):
+    def __init__(self, string: str):
+        if '/' in string:
+            lst = string.split('/')
+            self.__numerator = int(lst[0])
+            self.__denominator = int(lst[1])
+        elif '.' in string:
+            lst = string.split('.')
+            self.__numerator = int(lst[0]) * 10 ** len(lst[1]) + int(lst[1])
+            self.__denominator = 10 ** len(lst[1])
+        elif ',' in string:
+            lst = string.split(',')
+            self.__numerator = int(lst[0]) * 10 ** len(lst[1]) + int(lst[1])
+            self.__denominator = 10 ** len(lst[1])
+        if self.__numerator < 0 and self.__denominator < 0:
+            self.__numerator = self.__numerator * -1
+            self.__denominator = self.__denominator * -1
+        elif self.__denominator < 0:
+            self.__numerator = self.__numerator * -1
+            self.__denominator = self.__denominator * -1
 
-
-class NewFraction(Fraction):
     def __str__(self):
-        return f'{self._numerator}/{self._denominator}'
+        return f'{self.__numerator}/{self.__denominator}'
 
     def gcd(self):
         print("new Evklid")
@@ -14,28 +31,28 @@ class NewFraction(Fraction):
             r = mx % mn
             mx = mn
             mn = r
-        return NewFraction(int(self.__get_num() / mx), int(self.__get_den() / mx))
+        return NewFraction(str(int(self.__get_num() / mx)) + '/' + str(int(self.__get_den() / mx)))
 
-    def __get_num(self):
-        return self._numerator
+    def __get_num(self) -> int:
+        return self.__numerator
 
-    def __get_den(self):
-        return self._denominator
+    def __get_den(self) -> int:
+        return self.__denominator
 
     def __add__(self, other):
         print('new add')
-        return NewFraction(self.__get_num() * other.__get_den()+self.__get_den() * other.__get_num(),
-                           self.__get_den() * other.__get_den()).gcd()
+        return NewFraction(str(self.__get_num() * other.__get_den()+self.__get_den() * other.__get_num()) + '/' +
+                           str(self.__get_den() * other.__get_den())).gcd()
 
     def __sub__(self, other):
         print('new sub')
-        return NewFraction(self.__get_num() * other.__get_den() - self.__get_den() * other.__get_num(),
-                           self.__get_den() * other.__get_den()).gcd()
+        return NewFraction(str(self.__get_num() * other.__get_den() - self.__get_den() * other.__get_num()) + '/' +
+                           str(self.__get_den() * other.__get_den())).gcd()
 
     def __mul__(self, other):
         print('new mul')
-        return NewFraction(self.__get_num() * other.__get_num(), self.__get_den() * other.__get_den()).gcd()
+        return NewFraction(str(self.__get_num() * other.__get_num()) + '/' + str(self.__get_den() * other.__get_den())).gcd()
 
     def __truediv__(self, other):
         print('new div')
-        return NewFraction(self.__get_num() * other.__get_den(), self.__get_den() * other.__get_num()).gcd()
+        return NewFraction(str(self.__get_num() * other.__get_den()) + '/' + str(self.__get_den() * other.__get_num())).gcd()
